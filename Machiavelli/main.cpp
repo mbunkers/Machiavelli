4ef9d4d087b7888a62f8dbcf36383eb65b4966eb
Marc Bunkers <marcbunkers@icloud.com>
2014-12-10 09:53:03 +0100

    Initial Commit

---
+++ Machiavelli/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Machiavelli
+//
+//  Created by Marc Bunkers on 10/12/14.
+//  Copyright (c) 2014 Marc Bunkers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
