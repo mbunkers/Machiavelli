acca3b4dc2284a85833224e7ed4151b0a0dce38f
leonvantuyl <leonvtuyl@hotmail.com>
2014-12-10 10:38:21 +0100

    Initial commit from windows including server

---
+++ Machiavelli/Machiavelli.cpp
@@ -0,0 +1,12 @@
+// Machiavelli.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Machiavelli!\n";
+	return 0;
+}
+
